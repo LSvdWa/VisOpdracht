fabc993c09c0a87204f01c5ea6aebe51ce4ca0b8
LSvdWa <l.s.vanderwaal@students.uu.nl>
2026-01-20 10:49:27 +0100

    up

--- Visopdracht.R
+++ Visopdracht.R
@@ -13,7 +13,7 @@ ggplot(dataVI, aes(x=year, y=temp)) +
   geom_line() +
   geom_point()
 
-ggplot(tail(arrange(dataVI, temp), 30), aes(x=year, y=temp)) +
+ggplot(tail(dataVI, 30), aes(x=year, y=temp)) +
   geom_line() +
   geom_point() +
   geom_text(
@@ -22,11 +22,10 @@ ggplot(tail(arrange(dataVI, temp), 30), aes(x=year, y=temp)) +
     vjust = -1,
     colour = "black",
     size = 4
-  ) + theme_bw()
+  ) + theme_minimal()
 
 
 dataV <- read.csv("./Data2.csv")
-arrange(dataV, -X2022.2022)
 
 library(dplyr)
 library(tidyr)
@@ -34,18 +33,18 @@ library(ggplot2)
 
 df_long <- dataV %>%
   arrange(desc(X2022.2022)) %>%
-  slice(2:1) %>%
+  slice(2:17) %>%
   pivot_longer(
     cols = c(X2017.2017, X2022.2022),
     names_to = "Year",
     values_to = "Value"
   )
+library(forcats)
 
-ggplot(df_long, aes(x = Exports.by, y = Value, fill = Year)) +
-  geom_bar(stat = "identity", position = "dodge")
-
-ggplot(df_long, aes(x = reorder(Exports.by, -Value), y = Value, fill = Year)) +
+ggplot(df_long,
+       aes(x = fct_rev(reorder(Exports.by, Value)),
+           y = Value,
+           fill = Year)) +
   geom_bar(stat = "identity", position = "dodge") +
   labs(x = NULL, y = "Exports", fill = "Year") +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
-
