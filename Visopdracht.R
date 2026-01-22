2a47807afe64e95dfa07e7bb793c8f3558711dc3
LSvdWa <l.s.vanderwaal@students.uu.nl>
2026-01-22 15:49:49 +0100

    toevoegingen

--- Visopdracht.R
+++ Visopdracht.R
@@ -51,3 +51,34 @@ ggplot(df_long,
                     labels = c("2017", "2022")) +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
+
+Categorie <- c("Aantasting", "Verplichte compensatie", "Compensatie door project")
+Waardes <- c(8.26, 15.14, 33)
+df <- data.frame(Categorie, Waardes)
+df <- df %>%
+  mutate(Categorie = factor(Categorie, levels = Categorie[order(Waardes)]))
+
+ggplot(df, aes(x = Categorie, y = Waardes, fill = Categorie)) +
+  geom_bar(stat = "identity") +
+  scale_fill_manual(values = c("red", "skyblue", "green")) +
+  labs(x = "Categorie", y = "Waarde", title="Compensatie Natuurnetwerk Nederland") +
+  theme_minimal()
+
+data4 <- data.frame(
+  A = c("Pseudogene/ambiguous, 17%", "5' to known gene, 22%", "Novel, 24%", "Within or 3' flanking to a known gene, 36%"),
+  B = c(17, 22, 24, 36)
+)
+
+
+
+colors <- c("lightblue", "purple", "lightyellow", "maroon")
+
+pie(
+  data4$B,                     # values
+  labels = data4$A,            # labels
+  col = colors,                # colors
+  main = "Distribution of all TFBS Regions",
+  clockwise = TRUE,
+  cex = 0.8                     # optional: make labels smaller
+)
+
